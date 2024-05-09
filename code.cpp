5bfa398f90e18feda2bbb4767d91de28b0288261
7722094221 <134087625+7722094221@users.noreply.github.com>
2024-05-09 11:14:56 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,6 +1,6 @@
 
 
-#include <bits/stdc++.h> 
+#include <iostream> 
 using namespace std; 
 
 
@@ -33,11 +33,11 @@ int main(void)
 
 	
 	int x = 10; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
+	int n = sizeof(arr); 
 	int result = binarySearch(arr, 0, n - 1, x); 
-	(result == -1) 
-		? cout << "Element is not present in array"
-		: cout << "Element is present at index " << result; 
+	if(result == -1) 
+		cout << "Element is not present in array"
+		cout << "Element is present at index " << result; 
 
 	return 0; 
 }
